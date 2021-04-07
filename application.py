12f87f33739e88f1e4d43bc87dd369b8739a3e12
nabek <nabekabebe6@gmail.com>
2021-04-08 01:12:29 +0300

    Project structure set. forms added. page UI setup.

--- application.py
+++ application.py
@@ -1,12 +1,12 @@
 import os
 
-from flask import Flask, session
+from flask import Flask, session, render_template, url_for, redirect, flash, request
 from flask_session import Session
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
+from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 # Check for environment variable
 if not os.getenv("DATABASE_URL"):
     raise RuntimeError("DATABASE_URL is not set")
@@ -14,6 +14,7 @@ if not os.getenv("DATABASE_URL"):
 # Configure session to use filesystem
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
+app.config["SECRET_KEY"] = "769f213c4ea89e980132232fb84a528755cd1029110f2d274dfeabd6b404896a"
 Session(app)
 
 # Set up database
@@ -23,4 +24,33 @@ db = scoped_session(sessionmaker(bind=engine))
 
 @app.route("/")
 def index():
-    return "Project One: TODO"
+    return render_template("index.html", title="home page")
+
+
+@app.route('/register', methods=('GET', 'POST'))
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        print("nice")
+        flash(f'user {form.username.data} created successfully!', 'success')
+        redirect(url_for('index'))
+    else:
+        print("form error")
+        flash(f'Error creating user!', 'danger')
+        print(form)
+    return render_template('auth/register.html', title="Register", form=form)
+
+
+@app.route('/login', methods=('GET', 'POST'))
+def login():
+    form = LoginForm()
+    if form.validate():
+        print("nice")
+    else:
+        print("form error")
+    return render_template('auth/login.html', title="Login", form=form)
+
+
+@app.route('/logout', methods=('GET', 'POST'))
+def logout():
+    return redirect(url_for('index'))
---
+++ forms.py
@@ -0,0 +1,26 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired, Length, EqualTo
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Telegram Username', [DataRequired()])
+    email = StringField('Email Address', [Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        DataRequired(message="Can't be empty!"),
+        Length(min=6)
+    ])
+    confirm_password = PasswordField(
+        'Confirm Password', [EqualTo('password', message='passowrd must match!')])
+    register_btn = SubmitField('Sign up')
+
+
+class LoginForm(FlaskForm):
+    email = StringField('Email Address', [DataRequired(
+        message="Can't be empty!"), Length(min=6, max=35)])
+    password = PasswordField('New Password', [
+        DataRequired(message="Can't be empty!"),
+        Length(min=6)
+    ])
+    remember_me = BooleanField('Remember me')
+    login_btn = SubmitField('Sign In')
