5412cbb1135bd3ec0e8d93c599786adc48a43eb8
nabek <nabekabebe6@gmail.com>
2021-04-23 16:04:51 +0300

    ReadMe modified

--- run.py
+++ run.py
@@ -1,13 +1,4 @@
-from books.app import app
+from books import app
 
 if __name__ == "__main__":
     app.run(debug=True)
-
-
-# <form action = {{  # url_for('app.review', id=book.id if book.id else book.isbn if book.isbn else
-#     book.getBookAmazonLink())  # }} method="GET">
-#     {{reviewForm.hidden_tag()}}
-#     {{reviewForm.sort(onselect ="{{url_for('app.review', id=book.id if book.id else book.isbn if
-#                                            book.isbn else
-#                                            book.getBookAmazonLink())}}", class="form-select form-select-sm")}}
-#     < /form >
